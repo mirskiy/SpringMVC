3263b1485b5e5377dce93961660033fe257b347d
mirskiy <mirskiy@gmail.com>
2018-09-28 12:01:41 -0700

    shoulda just c/p'd instead of forking

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -1,31 +1,20 @@
 package com.mkyong.common.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
-public class HelloController {
-
-	@RequestMapping("/hello", method = RequestMethod.GET)
-	private String printWelcome(ModelMap model) {
-
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "hello";
-
-	}
-	
-	@RequestMapping("/2", method = RequestMethod.GET)
-	private String another(ModelMap model) {
-
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "hello";
-
-	}
-	
-	@RequestMapping("/greet", method = RequestMethod.GET)
-	private String greet(String greetee) {
-		return "test";
-	}
+@RestController
+@RequestMapping("/home")
+public class IndexController {
+  @RequestMapping("/")
+  String get(){
+    //mapped to hostname:port/home/
+    return "Hello from get";
+  }
+  @RequestMapping("/index")
+  String index(){
+    //mapped to hostname:port/home/index/
+    return "Hello from index";
+  }
 }

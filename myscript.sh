58de2bcf9d4ce73852d5b5b99582160d9511db44
mirskiy <mirskiy@gmail.com>
2018-11-19 11:59:17 -0800

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,4 +2,4 @@ echo Hello
 mkdir test-output || true
 #git clone https://github.com/mirskiy/heimdall-lite || true
 cp /home/tomcat/temp/heimdall-lite/heimdallite.html test-output/index.html
-cp /home/tomcat/temp/heimdall-lite/data/sample_json/rhel-test1.json test-output/test.json
+cp /home/tomcat/temp/heimdall-lite/data/sample_jsons/rhel-test1.json test-output/test.json
